f7b28b0955060e0836cf6dd79a2b652a3466e6b1
Tulun <jaykiraly@gmail.com>
2016-08-23 12:21:19 -0700

    Added authCtrl to index and auth config.

--- src/js/auth/index.js
+++ src/js/auth/index.js
@@ -7,4 +7,8 @@ let authModule = angular.module('app.auth',[]);
 import AuthConfig from './auth.config';
 authModule.config(AuthConfig);
 
+// Controllers
+import AuthCtrl from './auth.controller';
+authModule.controller('AuthCtrl', AuthCtrl);
+
 export default authModule;
