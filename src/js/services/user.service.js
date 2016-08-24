34914e6bd44e51798949523b4e5ae3ef93cd8eae
Tulun <jaykiraly@gmail.com>
2016-08-24 14:34:05 -0700

    Added service variables to User class.

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,7 +1,13 @@
 export default class User {
-  constructor() {
+  constructor(AppConstants, $http) {
     'ngInject';
 
+    // Add _ to variable names to denote services.
+    this._AppConstants = AppConstants;
+    this._$http = $http;
+
+    // Object to store our user properties.
+    this.current = null;
   }
-  
+
 }
